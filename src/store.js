db8dd11a7affb1716dc4efa05a6e4112463f9168
spoorthy1-reddy <jalespoorthyreddy@gmail.com>
2025-04-09 02:46:31 -0400

    Updated store.js to configure Redux store with cartReducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
+// Importing the necessary function from Redux Toolkit to configure the store
 import { configureStore } from '@reduxjs/toolkit';
+// Importing the cartReducer from the CartSlice.js to manage the cart state
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+// Configuring the Redux store and assigning the cartReducer to manage the 'cart' slice of state
+const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
+
+// Exporting the configured store to be used in the application
+export default store;
